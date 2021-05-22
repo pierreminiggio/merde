f1b3c8c25bfb4cf628e81af2a145e089b1be95b5
BeowolfK <65718242+BeowolfK@users.noreply.github.com>
2021-05-22 13:51:30 +0200

    Update index.php
    Simple modification for fixed div

--- index.php
+++ index.php
@@ -151,7 +151,9 @@ echo    '<head>
         z-index: -1;
     }
     .set{
+        position: fixed; !important
         position: absolute;
+        bottom:0;
         width: 100%;
         height: 100%;
         top: 0;
